de302dadf2211d032e4cb78c52c6552bfeb1c988
knvelasquez <knvelasquez@hotmail.com>
2023-02-22 20:16:27 -0300

    set unit test for Get a super hero by unique id

--- src/main/java/com/superhero/api/SuperHeroApi.java
+++ src/main/java/com/superhero/api/SuperHeroApi.java
@@ -1,10 +1,12 @@
 package com.superhero.api;
 
 import com.superhero.model.SuperHeroModel;
+
 import java.util.List;
 
 public interface SuperHeroApi {
 
     List<SuperHeroModel> getAll();
 
+    SuperHeroModel getByUniqueId(long id);
 }
--- src/test/java/com/superhero/SuperHeroTest.java
+++ src/test/java/com/superhero/SuperHeroTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,5 +34,29 @@ public class SuperHeroTest {
 
         //Assert
         assertTrue(listAllSuperHeroResult.size() > 0);
+    }
+
+    @Test
+    @DisplayName("Get a super hero information by unique id should return ok result")
+    public void getASuperHeroInformationByUniqueIdShouldReturnOkResult() {
+        //Arrange
+        when(superHeroApi.getByUniqueId(1L)).thenReturn(Mock.superMan);
+        when(superHeroApi.getByUniqueId(2L)).thenReturn(Mock.batMan);
+        when(superHeroApi.getByUniqueId(3L)).thenReturn(Mock.spiderMan);
+
+        //Act
+        SuperHeroModel superHeroResult1 = superHeroApi.getByUniqueId(1L);
+        SuperHeroModel superHeroResult2 = superHeroApi.getByUniqueId(2L);
+        SuperHeroModel superHeroResult3 = superHeroApi.getByUniqueId(3L);
+
+        //Assert
+        assertNotNull(superHeroResult1);
+        assertEquals("SuperMan", superHeroResult1.getName());
+
+        assertNotNull(superHeroResult2);
+        assertEquals("BatMan", superHeroResult2.getName());
+
+        assertNotNull(superHeroResult3);
+        assertEquals("SpiderMan", superHeroResult3.getName());
     }
 }
